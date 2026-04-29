24984455f5ceae76c5483cd7ab47ac561cf82c9e
Cauê Fernando de Almeida <cauefernando@gmail.com>
2026-04-29 20:03:31 -0300

    cleaning the project for a swap

--- src/app/app.config.server.ts
+++
@@ -1,5 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { appConfig } from './app.config';
-
-// SSR removed - using browser config only
-export const config = appConfig;
--- src/app/models/pet.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Pet {
-  name: string;
-  stage: 'newborn' | 'puppy' | 'young' | 'adult' | 'mature' | 'old';
-  daysWithoutNicotine: number;
-  moneysaved: number;
-  vcoins: number;
-}
